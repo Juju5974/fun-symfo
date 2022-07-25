f24e40121bee30ac2fe35aa00b68751846608a30
Juju5974 <juliette.verschoore@hotmail.fr>
2022-07-25 11:59:30 +0200

    first deploy cors 25.07.22

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -17,7 +17,7 @@ $('document').ready(() => {
         $.ajaxSetup({
             headers: {
                 'X-CSRF-TOKEN': $('input[name="_token"]').val(),
-                'Access-Control-Allow-Origin': 'https://radiant-hamlet-79456.herokuapp.com/', 'https://radiant-hamlet-79456.herokuapp.com/'
+                'Access-Control-Allow-Origin': '*'
             }
         });
         $.ajax({
@@ -50,7 +50,7 @@ $('document').ready(() => {
         $.ajaxSetup({
             headers: {
                 'X-CSRF-TOKEN': $('input[name="_token"]').val(),
-                'Access-Control-Allow-Origin': 'https://radiant-hamlet-79456.herokuapp.com/testvote'
+                'Access-Control-Allow-Origin': '*'
             }
         });
         $.ajax({
