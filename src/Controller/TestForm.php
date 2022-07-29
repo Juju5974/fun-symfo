8ead1681cddffa73ff484b35f9422239ee814dcd
Juju5974 <juliette.verschoore@hotmail.fr>
2022-07-29 12:41:13 +0200

    first deploy cors 29.07.22

--- src/Controller/TestForm.php
+++ src/Controller/TestForm.php
@@ -15,6 +15,7 @@ Class TestForm extends AbstractController {
     {
         header("Access-Control-Allow-Origin: *");
         header("Access-Control-Allow-Methods: POST");
+        header("HTTP/1.1 200 OK");
         // On créé l'entité $post avec les données fourni en POST
         $post = new Post();
         $post->setContent($request->request->get('content'));
@@ -32,6 +33,7 @@ Class TestForm extends AbstractController {
     {
         header("Access-Control-Allow-Origin: *");
         header("Access-Control-Allow-Methods: POST");
+        header("HTTP/1.1 200 OK");
         // On créé l'entité $post avec les données fourni en POST
         $vote = new Vote();
         $vote->setRating($request->request->get('rating'));
