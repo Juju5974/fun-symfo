9c448b2637116663f115132331defb0e3e88a673
Juju5974 <juliette.verschoore@hotmail.fr>
2022-08-02 11:08:46 +0200

    first deploy cors 02.08.22

--- src/Controller/TestForm.php
+++ src/Controller/TestForm.php
@@ -17,7 +17,7 @@ Class TestForm extends AbstractController {
         // On créé l'entité $post avec les données fourni en POST
         $post = new Post();
         $post->setContent($request->request->get('content'));
-
+        //
 
 		// On récupère la liste des erreurs de notre entité $user
 		$errors = $validator->validate($post);
